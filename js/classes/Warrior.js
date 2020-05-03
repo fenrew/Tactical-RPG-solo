ef7f1e065d3d451b6d2dd5352594b2d40ec46c84
unknown <markusheldrup@hotmail.com>
2020-05-03 13:21:35 +0200

    added hamstring spell

--- js/classes/Warrior.js
+++ js/classes/Warrior.js
@@ -1,89 +1,92 @@
 class Warrior extends BaseClass {
-    constructor(player) {
-      super()
-      this.className = "Warrior";
-      this.cssString = "warrior-player-area";
-      this.cssPlayerPanelString = "warrior-class-player-panel";
-      this.player = player;
-  
-      this.combatstats = {
-        hp: 450,
-        currentHp: 450,
-        mana: 120,
-        currentMana: 120,
-        initiation: 250,
-        maxMovementPoints: 5,
-        currentMovementPoints: 5,
-      };
-  
-      this.conditions = {
-        silenced: false,
-        disarmed: false,
-        onAttack: [],
-        onDefense: [],
-      };
-  
-      this.damageModifiers = {
-        defensive: {
+  constructor(player) {
+    super();
+    this.className = "Warrior";
+    this.cssString = "warrior-player-area";
+    this.cssPlayerPanelString = "warrior-class-player-panel";
+    this.player = player;
+
+    this.combatstats = {
+      hp: 450,
+      currentHp: 450,
+      mana: 120,
+      currentMana: 120,
+      initiation: 250,
+      maxMovementPoints: 5,
+      currentMovementPoints: 5,
+    };
+
+    this.conditions = {
+      silenced: false,
+      disarmed: false,
+      onAttack: [],
+      onDefense: [],
+    };
+
+    this.damageModifiers = {
+      defensive: {
+        allDamage: 1,
+        magicalDamage: {
+          allDamage: 1.1,
+          elementalMagic: {
+            fire: 1,
+            frost: 1,
+            arcane: 1,
+            holy: 1,
+            nature: 1,
+            darkness: 1,
+          },
+        },
+        physicalDamage: {
+          allDamage: 0.9,
+          rangedDamage: 1,
+          meleeDamage: 1,
+          bleeds: 1,
+        },
+      },
+      offensive: {
+        allDamage: 1,
+        magicalDamage: {
           allDamage: 1,
-          magicalDamage: {
-            allDamage: 1.1,
-            elementalMagic: {
-              fire: 1,
-              frost: 1,
-              arcane: 1,
-              holy: 1,
-              nature: 1,
-              darkness: 1,
-            },
-          },
-          physicalDamage: {
-            allDamage: 0.9,
-            rangedDamage: 1,
-            meleeDamage: 1,
-            bleeds: 1,
+          elementalMagic: {
+            fire: 1,
+            frost: 1,
+            arcane: 1,
+            holy: 1,
+            nature: 1,
+            darkness: 1,
           },
         },
-        offensive: {
+        physicalDamage: {
           allDamage: 1,
-          magicalDamage: {
-            allDamage: 1,
-            elementalMagic: {
-              fire: 1,
-              frost: 1,
-              arcane: 1,
-              holy: 1,
-              nature: 1,
-              darkness: 1,
-            },
-          },
-          physicalDamage: {
-            allDamage: 1,
-            rangedDamage: 1,
-            meleeDamage: 1,
-            bleeds: 1,
-          },
+          rangedDamage: 1,
+          meleeDamage: 1,
+          bleeds: 1,
         },
-        healing: {
-          recieved: 1,
-          casterModifier: 1,
-        },
-      };
-  
-      this.castCounter = {
-        defense: 0,
-        fighter: 0,
-        blood: 0,
-      };
-  
-      this.spells = {
-        slash: {
-          ...warriorSpellObject.slash,
-          spellInfo: { ...warriorSpellObject.slash.spellInfo },
-        },
-      };
-    }
-  
-    _checkIfPromotionToNewClass() {}
+      },
+      healing: {
+        recieved: 1,
+        casterModifier: 1,
+      },
+    };
+
+    this.castCounter = {
+      defense: 0,
+      fighter: 0,
+      blood: 0,
+    };
+
+    this.spells = {
+      slash: {
+        ...warriorSpellObject.slash,
+        spellInfo: { ...warriorSpellObject.slash.spellInfo },
+      },
+      hamstring: {
+        ...warriorSpellObject.hamstring,
+        spellInfo: { ...warriorSpellObject.hamstring.spellInfo },
+      },
+    };
   }
-  
+
+  _checkIfPromotionToNewClass() {}
+}
--- js/classes/warrior-spells/warrior-spells.js
+++ js/classes/warrior-spells/warrior-spells.js
@@ -1,41 +1,86 @@
 const warriorSpellObject = {
-    slash: {
-        id: "slash",
-        name: "Slash",
-        cast: (position, player) => {
-          player._addTargetSpellConditions(player.spells.slash, position, 2);
-        },
-        castEffect: (target, spell, player) => {
-          let modifiedDamage = Math.floor(
-            spell.spellInfo.damage *
-              calculatePhysicalMeleeDamageModifiers(player.player, target)
-          );
-          target.class.combatstats.currentHp -= modifiedDamage;
-          return modifiedDamage;
-        },
-        spellInfo: {
-          learned: true,
-          canBeCast: true,
-          type: "damage",
-          source: "physical-melee",
-          manaCost: 30,
-          damage: 20,
-          freeCells: true,
-          straigthLine: false,
-          diagonal: false,
-          areaOfEffect: 1,
-          minRange: 1,
-          maxRange: 1,
-          modifiableRange: false,
-          lineOfSight: false,
-          cooldown: false,
-          castsPerTurn: 2,
-          conditionsRequirements: {
-            disarmed: true,
-          },
-        },
-        category: "warrior",
-        toLearn: 0,
-        castCounter: 0,
+  //FIGHTER
+  slash: {
+    id: "slash",
+    name: "Slash",
+    cast: (position, player) => {
+      player._addTargetSpellConditions(player.spells.slash, position, 2);
+    },
+    castEffect: (target, spell, player) => {
+      let modifiedDamage = Math.floor(
+        spell.spellInfo.damage *
+          calculatePhysicalMeleeDamageModifiers(player.player, target)
+      );
+      target.class.combatstats.currentHp -= modifiedDamage;
+      return modifiedDamage;
+    },
+    spellInfo: {
+      learned: true,
+      canBeCast: true,
+      type: "damage",
+      source: "physical-melee",
+      manaCost: 30,
+      damage: 20,
+      freeCells: true,
+      straigthLine: false,
+      diagonal: false,
+      areaOfEffect: 1,
+      minRange: 1,
+      maxRange: 1,
+      modifiableRange: false,
+      lineOfSight: false,
+      cooldown: false,
+      castsPerTurn: 2,
+      conditionsRequirements: {
+        disarmed: true,
       },
+    },
+    category: "fighter",
+    toLearn: 0,
+    castCounter: 0,
+  },
+  hamstring: {
+    id: "hamstring",
+    name: "Hamstring",
+    cast: (position, player) => {
+      player._addTargetSpellConditions(player.spells.hamstring, position);
+    },
+    castEffect: (target, spell, player) => {
+      let modifiedDamage = Math.floor(
+        spell.spellInfo.damage *
+          calculatePhysicalMeleeDamageModifiers(player.player, target)
+      );
+
+      target.class.combatstats.currentHp -= modifiedDamage;
+      target.class.combatstats.currentMovementPoints -= 2;
+
+      handleSpellDamageEffectAnimation(target, 2, "mp");
+
+      return modifiedDamage;
+    },
+    spellInfo: {
+      learned: true,
+      canBeCast: true,
+      type: "damage",
+      source: "physical-melee",
+      manaCost: 30,
+      damage: 10,
+      freeCells: true,
+      straigthLine: false,
+      diagonal: false,
+      areaOfEffect: 1,
+      minRange: 1,
+      maxRange: 1,
+      modifiableRange: false,
+      lineOfSight: false,
+      cooldown: false,
+      castsPerTurn: 2,
+      conditionsRequirements: {
+        disarmed: true,
+      },
+    },
+    category: "fighter",
+    toLearn: 0,
+    castCounter: 0,
+  },
 };
