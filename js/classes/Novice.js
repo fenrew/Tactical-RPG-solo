c4744e9db7ac094038fc50f9a7177097a86977ce
unknown <markusheldrup@hotmail.com>
2020-05-03 11:56:19 +0200

    created baseclass

--- js/classes/Novice.js
+++ js/classes/Novice.js
@@ -1,4 +1,4 @@
-class Novice {
+class Novice extends BaseClass {
   constructor(player) {
     this.className = "Novice";
     this.cssString = "novice-player-area";
@@ -148,57 +148,6 @@ class Novice {
         spellInfo: { ...noviceSpellObject.holySpike.spellInfo },
       },
     };
-  }
-
-  async _addTargetSpellConditions(spell, position) {
-    if (checkIfSpellIsCastable(this, spell)) return console.log("Cant be cast");
-    let target = Game._getUnitByPosition(position);
-    if (!target && !spell.spellInfo.castOnNoTarget)
-      return console.log("No target");
-
-    this.combatstats.currentMana -= spell.spellInfo.manaCost;
-    updateCurrentManaBar(this.player);
-
-    const onAttackAnswers = {cancelSpell: false}
-    this.conditions.onAttack.forEach((ele) => {
-      let answerOnAttack = ele.spell.conditionEffect(this.player, spell, ele);
-      if(answerOnAttack) {
-        for (let key in answerOnAttack){
-          onAttackAnswers[key] = answerOnAttack[key]
-        }
-      }
-    });
-    if(onAttackAnswers.cancelSpell) return
-
-    spell.castCounter += 1;
-    this.castCounter[spell.category] += 1;
-    this._checkIfNewSpellIsLearned(spell);
-
-    let modifiedDamage = await spell.castEffect(
-      target ? target : position,
-      spell,
-      this
-    );
-
-    if (noviceSpellAnimations[spell.id])
-      await noviceSpellAnimations[spell.id](target, this.player);
-
-    if (target?.newPosition) {
-      target.position = { ...target.newPosition };
-      target.newPosition = false;
-    }
-
-    if (modifiedDamage) {
-      handleSpellDamageEffectAnimation(
-        target,
-        modifiedDamage,
-        spell.spellInfo.type
-      );
-    }
-
-    Game._checkIfAnyoneHasDied();
-
-    this._checkIfPromotionToNewClass();
   }
 
   _checkIfNewSpellIsLearned() {
--- js/classes/Sorcerer.js
+++ js/classes/Sorcerer.js
@@ -1,5 +1,6 @@
-class Sorcerer {
+class Sorcerer extends BaseClass {
   constructor(player) {
+    super()
     this.className = "Sorcerer";
     this.cssString = "wizard-player-area";
     this.cssPlayerPanelString = "wizard-class-player-panel";
@@ -139,69 +140,6 @@ class Sorcerer {
         spellInfo: { ...sorcererSpellObject.teleport.spellInfo },
       },
     };
-  }
-
-  async _addTargetSpellConditions(spell, position) {
-    if (checkIfSpellIsCastable(this, spell)) return console.log("Cant be cast");
-    let target = Game._getUnitByPosition(position);
-    if (!target && !spell.spellInfo.castOnNoTarget)
-      return console.log("No target");
-
-    this.combatstats.currentMana -= spell.spellInfo.manaCost;
-    updateCurrentManaBar(this.player);
-
-    
-    const onAttackAnswers = {cancelSpell: false}
-    this.conditions.onAttack.forEach((ele) => {
-      let answerOnAttack = ele.spell.conditionEffect(this.player, spell, ele, target);
-      if(answerOnAttack) {
-        for (let key in answerOnAttack){
-          onAttackAnswers[key] = answerOnAttack[key]
-        }
-      }
-    });
-    if(onAttackAnswers.cancelSpell) return
-
-    const onDefenseAnswers = {cancelSpell: false}
-    target.class.conditions.onDefense.forEach((ele) => {
-      let answerOnDefense = ele.spell.conditionEffect(this.player, spell, ele, target);
-      if(answerOnDefense) {
-        for (let key in answerOnDefense){
-          onDefenseAnswers[key] = answerOnDefense[key]
-        }
-      }
-    });
-   // if(onAttackAnswers.cancelSpell) return
-
-    spell.castCounter += 1;
-    this.castCounter[spell.category] += 1;
-    this._checkIfNewSpellIsLearned(spell);
-
-    let modifiedDamage = await spell.castEffect(
-      target ? target : position,
-      spell,
-      this
-    );
-
-    if (noviceSpellAnimations[spell.id])
-      await noviceSpellAnimations[spell.id](target, this.player);
-
-    if (target?.newPosition) {
-      target.position = { ...target.newPosition };
-      target.newPosition = false;
-    }
-
-    if (modifiedDamage) {
-      handleSpellDamageEffectAnimation(
-        target,
-        modifiedDamage,
-        spell.spellInfo.type
-      );
-    }
-
-    Game._checkIfAnyoneHasDied();
-
-    this._checkIfPromotionToNewClass();
   }
 
   _checkIfNewSpellIsLearned() {
