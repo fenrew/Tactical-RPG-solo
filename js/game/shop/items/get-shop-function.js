740a186887e9c5009149b807f0379804ab2ec11e
Markus Heldrup <markusheldrup@hotmail.com>
2020-05-17 01:17:50 +0200

    added shoulders to items

--- js/game/shop/items/get-shop-function.js
+++ js/game/shop/items/get-shop-function.js
@@ -1,4 +1,5 @@
 const getShop = (shopNumber) => {
   if (shopNumber === 201) return itemWeaponsList;
   else if (shopNumber === 202) return;
+  else if (shopNumber === 203) return itemShouldersList;
 };
---
+++ js/game/shop/items/weaponry/shoulders/item-shoulders.js
@@ -0,0 +1,21 @@
+const itemShouldersList = {
+  shouldersOfMagic: {
+    name: "Shoulders of Magic",
+    price: 100,
+    type: "shoulders",
+    stats: [
+      {
+        keySequence: "offAllDamageM",
+        value: 0.2,
+      },
+      {
+        keySequence: "hp",
+        value: 10,
+      },
+      {
+        keySequence: "mana",
+        value: 25,
+      },
+    ],
+  },
+};
