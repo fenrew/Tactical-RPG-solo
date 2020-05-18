4676b8bec0467b517e84bc8f19bfc363b3f787ac
Markus Heldrup <markusheldrup@hotmail.com>
2020-05-18 12:12:04 +0200

    added chests to items list

--- js/game/shop/items/get-shop-function.js
+++ js/game/shop/items/get-shop-function.js
@@ -2,7 +2,7 @@ const getShop = (shopNumber) => {
   if (shopNumber === 201) return itemWeaponsList;
   else if (shopNumber === 202) return itemHelmetsList;
   else if (shopNumber === 203) return itemShouldersList;
-  else if (shopNumber === 204) return;
+  else if (shopNumber === 204) return itemChestList;
   else if (shopNumber === 205) return itemLegsList;
   else if (shopNumber === 206) return itemFeetList;
 };
---
+++ js/game/shop/items/weaponry/chests/item-chests.js
@@ -0,0 +1,17 @@
+const itemChestList = {
+  ironChainmail: {
+    name: "Iron Chainmail",
+    price: 100,
+    type: "chest",
+    stats: [
+      {
+        keySequence: "mana",
+        value: 10,
+      },
+      {
+        keySequence: "hp",
+        value: 20,
+      },
+    ],
+  },
+};
