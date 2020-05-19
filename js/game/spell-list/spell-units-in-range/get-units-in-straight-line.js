1b6abeee5e222e3131f2e18ef4b455ba6ca778ac
Markus Heldrup <markusheldrup@hotmail.com>
2020-05-19 09:31:51 +0200

    fixed getUnitsInStraightLine function

--- js/game/spell-list/spell-units-in-range/get-units-in-straight-line.js
+++ js/game/spell-list/spell-units-in-range/get-units-in-straight-line.js
@@ -1,19 +1,10 @@
 // Takes two positions and return every unit in a straight line between those two positions
 const getUnitsInStraightLine = (fromPos, toPos) => {
-  const differencePos = { y: fromPos.y - toPos.y, x: fromPos.x - toPos.x };
   const units = Game.combatTimeline.filter((ele) => {
-    if (differencePos.y < 0 || differencePos.x < 0) {
-      return (
-        (ele.position.y <= toPos.y && ele.position.y > fromPos.y) ||
-        (ele.position.x <= toPos.x && ele.position.x > fromPos.x)
-      );
-    } else {
-      return (
-        (ele.position.y >= toPos.y && ele.position.y < fromPos.y) ||
-        (ele.position.x >= toPos.x && ele.position.x < fromPos.x)
-      );
-    }
+    return (
+      (ele.position.y - fromPos.y) * (ele.position.y - toPos.y) <= 0 &&
+      (ele.position.x - fromPos.x) * (ele.position.x - toPos.x) <= 0
+    );
   });
-
   return units;
 };
