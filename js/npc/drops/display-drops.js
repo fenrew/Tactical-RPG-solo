c1698d68837ffaeee68b9976068e708eaaa7bec2
Markus Heldrup <markusheldrup@hotmail.com>
2020-05-24 12:04:21 +0200

    completed visualizeDropWindowHelper

--- js/npc/drops/display-drops.js
+++ js/npc/drops/display-drops.js
@@ -2,41 +2,62 @@ const displayDrops = (npc) => {
   const { dropTable } = npc.class;
   const { items } = dropTable;
 
-  const goldDrop = Math.floor(
+  const goldValue = Math.floor(
     Math.random() * (dropTable.coins[1] - dropTable.coins[0]) +
       dropTable.coins[0]
   );
-  console.log(goldDrop);
+
+  const goldDrop = {
+    name: "Gold",
+    value: goldValue,
+  };
 
   itemDrops = items
     .map((item) => (Math.random() <= item.dropChance ? item : ""))
     .filter((ele) => ele);
+
+  visualizeDropWindow(npc, goldDrop, itemDrops);
 };
 
-const visualizeDropWindow = (gold, items) => {
+const visualizeDropWindow = (npc, gold, items) => {
   const mainContainer = document.createElement("div");
-  const eachDropContainer = document.createElement("div");
-  const goldTitle = document.createElement("div");
-  const goldValue = document.createElement("div");
-  const goldIcon = document.createElement("div");
 
-  goldTitle.classList.add("goldTitle");
-  goldValue.classList.add("goldValue");
+  mainContainer.appendChild(visualizeDropWindow(gold));
+  items.forEach((ele) => {
+    mainContainer.appendChild(visualizeDropWindow(ele));
+  });
 
-  eachDropContainer.appendChild(goldTitle);
+  mainContainer.style.top = npc.position.y;
+  mainContainer.style.left = npc.position.x;
+
+  document.getElementById("player-area").appendChild(mainContainer);
 };
 
-const visualizeDropWindowHelper = (className, mainContainer) => {
+const visualizeDropWindowHelper = (item) => {
   const eachDropContainer = document.createElement("div");
   const dropTitle = document.createElement("div");
   const dropValue = document.createElement("div");
   const dropIcon = document.createElement("div");
 
-  dropTitle.classList.add("goldTitle");
-  dropValue.classList.add("goldValue");
-  dropIcon.classList.add("goldValue");
+  eachDropContainer.classList.add("drop-window-each-drop-container");
+  dropTitle.classList.add(
+    "drop-window-title",
+    "drop-window-title-" + item.name.replace(/\s/g, "-")
+  );
+  dropValue.classList.add(
+    "drop-window-value",
+    "drop-window-value-" + item.name.replace(/\s/g, "-")
+  );
+  dropIcon.classList.add(
+    "drop-window-icon",
+    "drop-window-icon-" + item.name.replace(/\s/g, "-")
+  );
+
+  dropTitle.innerText = item.name;
+  dropValue.innerText = item.price;
 
   eachDropContainer.appendChild(dropTitle);
   eachDropContainer.appendChild(dropValue);
   eachDropContainer.appendChild(dropIcon);
+  return eachDropContainer;
 };
